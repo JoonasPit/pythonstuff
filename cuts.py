4530cf061acb20274fb5314c876218cd0ef3e93e
Joonas <joonasmpitkaniemi@gmail.com>
2019-01-18 09:39:39 +0200

    created cuts.py

---
+++ cuts.py
@@ -0,0 +1,4 @@
+variable_cut = "Hattukauppias"
+print("Muuttujan 4 ensimmäistä kirjainta ovat",variable_cut[0:4])
+print("Muuttujan 4 viimeistä kirjainta ovat",variable_cut[-4:13])
+print("Muuttujan teksti väärinpäin ",variable_cut[::-1])
